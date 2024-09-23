a8a93d1a5f9c0cb565ce7627f6cad40f699cc492
syrene <144994323+shb1383@users.noreply.github.com>
2024-09-23 13:17:44 +0900

    updated score title for wrong answer

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -75,7 +75,7 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "Correct"
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
         }
         
         showingScore = true
